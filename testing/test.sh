765534cf72a12e3d0b769bef7581b73342f8b835
Willi Ballenthin <willi.ballenthin@gmail.com>
2012-07-21 17:40:51 -0400

    merge master into testing on test runs

--- testing/test.sh
+++ testing/test.sh
@@ -5,7 +5,12 @@ if [[ ! -d ./warehouse ]]; then
 fi
 
 
+git checkout testing || { exit 1; };
+git merge master;
 for file in TEST*.sh; do
     zsh $file;
 done;
+git add -u;
+git commit -m "testing run completing.";
+git checkout master;
 
