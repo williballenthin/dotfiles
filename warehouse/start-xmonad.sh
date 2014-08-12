501f12c96abf6bdfb9739aee4b4b4fc224587ea2
Willi Ballenthin <william.ballenthin@mandiant.com>
2014-08-11 22:23:47 -0400

    start-xmonad: dzen2 should be in corner

--- warehouse/start-xmonad.sh
+++ warehouse/start-xmonad.sh
@@ -4,7 +4,7 @@ xmodmap ~/.Xmodmap  # __XMODMAP__
 compton -b  # __COMPTON__
 bash ~/.wallpaper/wallpaper.sh &  # __WALLPAPER__
 xautolock -time 5 -locker 'i3lock -g' &
-(bash ~/.xmonad/bar.sh | dzen2 -ta r -fn 'Droid Sans Mono-7') &
+(bash ~/.xmonad/bar.sh | dzen2 -geometry -600+0 -bg black -fn 'Droid Sans Mono-7') &
 trayer --edge top --align left --widthtype request --height 15 &
 killall xfce4-notifyd  # sorry, but there's no associated "autostart" file per-user
 dunst -config ~/.dunstrc &
