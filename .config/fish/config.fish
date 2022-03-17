139040eaec2e82606ab059391fa68ecabd8cbe39
Willi Ballenthin <willi.ballenthin@gmail.com>
2022-03-17 12:27:39 -0600

    fish: add ipytest alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,6 +10,8 @@ if status --is-interactive
     abbr --add --global lt exa --tree --level=2 --long --git
     abbr --add --global gs tig status
     abbr --add --global cat bat
+    # via: https://stackoverflow.com/a/59069793/87207
+    abbr --add --global ipytest pytest --pdb --pdbcls=IPython.terminal.debugger:TerminalPdb
 end
 
 fish_ssh_agent
