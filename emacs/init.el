0e27ce1d585a2bcab1a1b9196cb0b6f37b6da14f
Willi Ballenthin <willi.ballenthin@gmail.com>
2024-01-15 11:07:57 +0000

    emacs: lsp config

--- emacs/init.el
+++ emacs/init.el
@@ -281,3 +281,35 @@
 (use-package nerd-icons)
 
 (use-package symbol-overlay)
+
+(use-package company
+  :hook
+  (prog-mode . company-mode)
+
+  :bind
+  (:map company-active-map
+    ("<tab>" . company-complete-selection))
+
+  :custom
+  (company-minimum-prefix-length 3)
+  (company-idle-delay 0.1))
+
+(use-package lsp-pyright
+  :ensure t
+  :hook (python-mode . (lambda () (require 'lsp-pyright))))
+
+(use-package lsp-mode
+  :after
+  lsp-pyright
+  :config
+  (general-def 'normal lsp-mode :definer 'minor-mode
+      "SPC l" lsp-command-map)
+  :hook ((prog-mode . lsp)
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp
+  :custom
+  (help-at-pt-timer-delay 0.9)
+  (help-at-pt-display-when-idle '(flymake-overlay)))
+
+(use-package lsp-ui :commands lsp-ui-mode)
+(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
