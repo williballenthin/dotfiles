ba833dd170ad251b32e8ab366d5f5596028d72d1
Willi Ballenthin <willi.ballenthin@gmail.com>
2024-01-15 11:04:59 +0000

    emacs: remove hl-mode hooks (missing symbol)

--- emacs/init.el
+++ emacs/init.el
@@ -239,9 +239,6 @@
   (evil-collection-init))
 
 (use-package diff-hl
-  :init
-  (add-hook 'magit-pre-refresh-hook 'diff-hl-magit-pre-refresh)
-  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
   :config
   (global-diff-hl-mode))
 
