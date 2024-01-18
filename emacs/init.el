f0706350001751c211c42c5af58efd4da97678c3
Willi Ballenthin <willi.ballenthin@gmail.com>
2024-01-18 20:31:06 +0000

    emacs: add tree-sitter config

--- emacs/init.el
+++ emacs/init.el
@@ -440,3 +440,18 @@
 
 (use-package lsp-ui :commands lsp-ui-mode)
 (use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
+
+(use-package tree-sitter
+  :ensure t
+  :config
+  ;; activate tree-sitter on any buffer containing code for which it has a parser available
+  (global-tree-sitter-mode)
+  ;; you can easily see the difference tree-sitter-hl-mode makes for python, ts or tsx
+  ;; by switching on and off
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+
+(use-package tree-sitter-langs
+  :ensure t
+  :after tree-sitter;
+  :config
+  (tree-sitter-langs-install-grammars)
