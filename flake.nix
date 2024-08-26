a8cf7290fc31d1427a724c5c4855a0956a64a220
Willi Ballenthin <willi.ballenthin@gmail.com>
2024-08-26 12:01:41 +0000

    flake: add jj config

--- flake.nix
+++ flake.nix
@@ -131,6 +131,7 @@
               home.file.".config/fish/functions/fzf.fish".source = ./.config/fish/functions/fzf.fish;
               home.file.".config/atuin/config.toml".source = ./.config/atuin/config.toml;
               home.file.".config/bat/config".source = ./.config/bat/config;
+              home.file.".config/jj/config.toml".source = ./.config/jj/config.toml;
           })
         ] ++ localModules;
       };
