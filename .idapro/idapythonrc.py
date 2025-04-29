c9693c5893de55790e265027e68741c9282338b9
Willi Ballenthin <willi.ballenthin@gmail.com>
2025-04-29 10:40:58 +0200

    idapythonrc: better detect when running within IDA

--- .idapro/idapythonrc.py
+++ .idapro/idapythonrc.py
@@ -1,38 +1,40 @@
-###############################################################################
-# Activate virtualenv at ~/.idapro/venv
-#
-# see also: https://williballenthin.com/post/using-a-virtualenv-for-idapython/
- 
-from pathlib import Path
+import os
 
-import idaapi
+if os.environ.get("IDA_IS_INTERACTIVE") == "1":
+    # when running within IDA Pro...
+    # see: https://community.hex-rays.com/t/how-to-check-if-idapythonrc-py-is-running-in-ida-pro-or-idalib/297/2
 
-def activate_virtualenv(virtualenv_path: Path):
-    for bindir in ("Scripts", "bin"):
-        activate_this_path = virtualenv_path / bindir / "activate_this.py"
+    ###############################################################################
+    # Activate virtualenv at ~/.idapro/venv
+    #
+    # see also: https://williballenthin.com/post/using-a-virtualenv-for-idapython/
+    from pathlib import Path
 
-        if not activate_this_path.exists():
-            continue
+    import idaapi
 
-        if not activate_this_path.is_file():
-            continue
+    def activate_virtualenv(virtualenv_path: Path):
+        for bindir in ("Scripts", "bin"):
+            activate_this_path = virtualenv_path / bindir / "activate_this.py"
 
-        exec(activate_this_path.read_text(), dict(__file__=str(activate_this_path)))
-        print("activated virtual environment: " + str(virtualenv_path))
-        break
+            if not activate_this_path.exists():
+                continue
 
-    else:
-        raise ValueError('Could not find "activate_this.py" in ' + str(virtualenv_path))
+            if not activate_this_path.is_file():
+                continue
+
+            exec(activate_this_path.read_text(), dict(__file__=str(activate_this_path)))
+            print("activated virtual environment: " + str(virtualenv_path))
+            break
+
+        else:
+            raise ValueError('Could not find "activate_this.py" in ' + str(virtualenv_path))
 
 
-if IDAPRO_OWNING_INTERPRETER:
     IDAUSR = Path(idaapi.get_user_idadir())
     activate_virtualenv(IDAUSR / "venv")
 
 
-###############################################################################
-# Configure logging
-if IDAPRO_OWNING_INTERPRETER:
+    ###############################################################################
+    # Configure logging
     import logging
     logging.basicConfig(level=logging.INFO)
-
